fa90a2b9810458df705147102aa3432238809366
ReeniReddyPinninti <90337184+ReeniReddyPinninti@users.noreply.github.com>
2024-11-15 16:27:10 +0530

    Sample maven java

---
+++ src/test/java/com/app/demo/SampleMavenJavaProject/AppTest.java
@@ -0,0 +1,29 @@
+package com.app.demo.SampleMavenJavaProject;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import org.junit.Test;
+//
+///**
+// * Unit test for simple App.
+// */
+//public class AppTest 
+//{
+//    /**
+//     * Rigorous Test :-)
+//     */
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+//}
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
